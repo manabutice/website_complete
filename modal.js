6dea7c66b8b4532e29714d2bda1a753325eb0f36
manabutice <manabutice@gmail.com>
2022-11-21 21:52:35 +0900

    self-introductionをindex.htmlに変更

--- modal.js
+++ modal.js
@@ -1,20 +1,26 @@
-const modal = document.querySelector("#myModal");
-const btn = document.querySelectorAll(".btn");
-const closeModal = document.getElementsByClassName("modalClose")[0];
+const btns = document.querySelectorAll(".btn");
+const modals = document.getElementsByClassName("modal");
+const closeModals = document.getElementsByClassName("modalClose");
 
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function () {
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    closeAllModal(); // 念の為全てのモーダルを閉じます
+    const modalId = btns[i].dataset.modal_id; // ボタンに設定されたモーダルを開きます
+    const modal = document.getElementById(modalId);
     modal.style.display = "block";
   });
 }
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-closeModal.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+
+// 閉じるボタンが押されたら全てのモーダルを閉じます
+for (let i = 0; i < closeModals.length; i++) {
+  closeModals[i].addEventListener("click", function () {
+    closeAllModal();
+  });
+}
+
+// 全てのモーダルを閉じます
+function closeAllModal() {
+  for (let i = 0; i < modals.length; i++) {
+    modals[i].style.display = "none";
   }
-};
+}
